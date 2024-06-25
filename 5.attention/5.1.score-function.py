8dffd720733cfe8b6b872ce91dd7ca7b5c22b64e
Caesarr007 <1094226253@qq.com>
2024-06-25 18:11:32 +0800

    score

--- 5.attention/5.1.score-function.py
+++ 5.attention/5.1.score-function.py
@@ -12,7 +12,7 @@ class AdditiveAttention(nn.Module):
         self.W_v = nn.Linear(hidden_size, 1, bias=False)
         self.dropout = nn.Dropout(dropout)
 
-    def masked_softmax(score, mask):
+    def masked_softmax(self, score, mask):
         score = score.masked_fill(mask, float('-inf'))
         return nn.functional.softmax(score, dim=-1)
 
@@ -23,10 +23,12 @@ class AdditiveAttention(nn.Module):
         # query: [batch_size, query_len, 1, hidden_size]
         # key: [batch_size, 1, key_len, hidden_size]
         features = query.unsqueeze(2) + key.unsqueeze(1)
+        print("features shape", features.shape)
         features = torch.tanh(features)
 
         score = self.W_v(features).squeeze(-1)
-        self.attention_weights = masked_softmax(score, mask)
+        print("score shape", score.shape)
+        self.attention_weights = self.masked_softmax(score, mask)
 
         return torch.bmm(self.dropout(self.attention_weights), value)
     
@@ -36,12 +38,12 @@ if __name__ == '__main__':
     batch_size = 2
     key_size = 3
     query_size = 3
-    hidden_size = 4
+    hidden_size = 3
     dropout = 0.1
-    key = torch.randn(batch_size, 5, key_size)
+    key = torch.randn(batch_size, 4, key_size)
     query = torch.randn(batch_size, 5, query_size)
-    value = torch.randn(batch_size, 5, hidden_size)
-    mask = torch.zeros(batch_size, 5, 5).bool()
+    value = torch.randn(batch_size, 4, hidden_size)
+    mask = torch.zeros(batch_size, 5, 4).bool()
     attention = AdditiveAttention(key_size, query_size, hidden_size, dropout)
     output = attention(key, query, value, mask)
     print(output)
