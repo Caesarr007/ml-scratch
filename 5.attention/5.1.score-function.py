2dc64e1474dc9585d3628a1d1d2050d92aa40787
Caesarr007 <1094226253@qq.com>
2024-06-26 17:38:53 +0800

    score-function

--- 5.attention/5.1.score-function.py
+++ 5.attention/5.1.score-function.py
@@ -33,17 +33,54 @@ class AdditiveAttention(nn.Module):
         return torch.bmm(self.dropout(self.attention_weights), value)
     
 
+class DotProductAttention(nn.Module):
+    def __init__(self, dropout):
+        super(DotProductAttention, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+
+    def masked_softmax(self, score, mask):
+        score = score.masked_fill(mask, float('-inf'))
+        return nn.functional.softmax(score, dim=-1)
+
+    def forward(self, query, key, value, mask):
+        # query: [batch_size, query_len, hidden_size]
+        # key: [batch_size, kv_len, hidden_size]
+        # value: [batch_size, kv_len, hidden_size]
+        d = query.shape[-1]
+        score = einsum('bqd,bkd->bqk', query, key) / d ** 0.5
+        scores = torch.bmm(queries, keys.transpose(1,2)) / d ** 0.5
+        print("score", score)
+        print(score.shape)
+        print("scores", scores)
+        print(scores.shape)
+        self.attention_weights = self.masked_softmax(score, mask)
+        print("attention_weights", self.attention_weights)
+        return einsum('bqk,bkd->bqd', self.dropout(self.attention_weights), value)
+    
+
 if __name__ == '__main__':
     # 测试加性注意力
-    batch_size = 2
-    key_size = 3
-    query_size = 3
-    hidden_size = 3
-    dropout = 0.1
-    key = torch.randn(batch_size, 4, key_size)
-    query = torch.randn(batch_size, 5, query_size)
-    value = torch.randn(batch_size, 4, hidden_size)
-    mask = torch.zeros(batch_size, 5, 4).bool()
-    attention = AdditiveAttention(key_size, query_size, hidden_size, dropout)
-    output = attention(key, query, value, mask)
-    print(output)
+    # batch_size = 2
+    # key_size = 3
+    # query_size = 3
+    # hidden_size = 3
+    # dropout = 0.1
+    # key = torch.randn(batch_size, 4, key_size)
+    # query = torch.randn(batch_size, 5, query_size)
+    # value = torch.randn(batch_size, 4, hidden_size)
+    # # mask 的形状 [batch_size, query_len, key_len]
+    # mask = torch.zeros(batch_size, 5, 4).bool()
+    # attention = AdditiveAttention(key_size, query_size, hidden_size, dropout)
+    # output = attention(key, query, value, mask)
+    # print(output)
+
+    # 测试缩放点积注意力
+    queries, keys = torch.normal(0, 1, (2, 1, 2)), torch.ones((2, 10, 2))
+    # values的小批量，两个值矩阵是相同的
+    values = torch.arange(40, dtype=torch.float32).reshape(1, 10, 4).repeat(
+        2, 1, 1)
+    print("values shape", values.shape)
+    mask = torch.zeros(2, 1, 10).bool()
+    attention = DotProductAttention(dropout=0.5)
+    attention.eval() 
+    print(attention(queries, keys, values, mask))
